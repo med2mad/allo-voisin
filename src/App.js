45de10b2bc9df8b5c351e44d2726c4443276e1c3
watawata <mohamed.leghdaich@gmail.com>
2023-09-20 04:29:06 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const s = 'dsadsad';
+  const b = true;
+  const a = [1, 2, 3];
+  const o = {o:'o'};
+  const n = 5;
+  
+  let [v, setV] = useState([{o:1}]);
+  const f=()=>{setV('false')}
+
   return (
     <div className="App App-header">
-      a
+      <p style={{color:"red"}} >{s}</p>
+      <p>{a}</p>
+      <p>{n}</p>
+      <p>{b}</p>
+      <p>{o.o}</p>
+      <p>{11}</p>
+      <p>{v}</p>
+      <p><button onClick={f}>click</button></p>
     </div>
   );
 }
