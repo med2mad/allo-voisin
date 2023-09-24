6d0b31d2dd77aa92cf0dbb57fe2440bd53b0e526
watawata <mohamed.leghdaich@gmail.com>
2023-09-24 23:56:06 +0100

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Cc from "./components/Cc";
 
 function App() {
 
-  let [a, seta] = useState([{o:1},{o:22},{o:333},{o:4444}]);
-  let b = true;
-  let o={o:12};
+  const [a, seta] = useState([{o:1},{o:22},{o:333},{o:4444}]);
+  const b = true;
+  const o = {o:12};
 
   function handleDelete(p){
     const newblogs = a.filter( item => item.o !== p);
     seta(newblogs);
   }
+
+  useEffect(()=>{console.log('side effect');});
 
   return (
     <div className="App App-header">
